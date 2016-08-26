2a3f2802c34aa53e7c3386615a8bb0c085e1de89
Cleanderson Lobo <cleandersonlobo@gmail.com>
2016-08-26 05:06:59 -0300

    Bug Fixes

--- package.js
+++ package.js
@@ -2,16 +2,18 @@ Package.describe({
   name: 'cleandersonlobo:odometeor',
   summary: 'A wrapper for odometer by HubSpot',
   // Brief, one-line summary of the package.
-    version: '0.0.1',
+    version: '0.0.5',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cleandersonlobo/odometeor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-api.versionsFrom('1.4.1.1');
+
+api.addFiles('lib/odometer.js', 'client');
+api.addFiles('lib/textroller.js', 'client');
 api.addFiles('lib/themes/odometer-theme-car.css', 'client');
 api.addFiles('lib/themes/odometer-theme-default.css', 'client');
 api.addFiles('lib/themes/odometer-theme-digital.css', 'client');
@@ -19,5 +21,5 @@ api.addFiles('lib/themes/odometer-theme-minimal.css', 'client');
 api.addFiles('lib/themes/odometer-theme-plaza.css', 'client');
 api.addFiles('lib/themes/odometer-theme-slot-machine.css', 'client');
 api.addFiles('lib/themes/odometer-theme-train-station.css', 'client');
-api.export('Odometeor', 'client');
+
 });
